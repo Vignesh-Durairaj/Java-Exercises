a6eca992603959b24d7b2d98cf0ffe150328dcfd
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2018-09-06 07:53:41 +0530

    Added Implementations of the number compositions

---
+++ src/test/java/com/vikhi/exercise/NumberCompositionsTest.java
@@ -0,0 +1,56 @@
+package com.vikhi.exercise;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class NumberCompositionsTest {
+
+	private int spaceCovered;
+	
+	private BigInteger probabilityCount;
+	
+	private static NumberCompositions numberCompositions;
+
+	public NumberCompositionsTest(int spaceCovered, BigInteger probabilityCount) {
+		super();
+		this.spaceCovered = spaceCovered;
+		this.probabilityCount = probabilityCount;
+	}
+	
+	@BeforeClass
+	public static void init() {
+		numberCompositions = new NumberCompositions();
+	}
+	
+	@Parameters
+	public static Collection<Object[]> parameters() {
+		return Arrays.asList(new Object[][] {
+			{0, BigInteger.valueOf(1)},
+			{1, BigInteger.valueOf(1)},
+			{2, BigInteger.valueOf(2)},
+			{3, BigInteger.valueOf(4)},
+			{4, BigInteger.valueOf(8)},
+			{5, BigInteger.valueOf(16)},
+			{6, BigInteger.valueOf(32)},
+			{7, BigInteger.valueOf(63)},
+			{8, BigInteger.valueOf(125)},
+			{9, BigInteger.valueOf(248)},
+			{10, BigInteger.valueOf(492)}
+		});
+	}
+	
+	@Test
+	public void testDiceCompositions() {
+		BigInteger probabilityCount = numberCompositions.getDiceCompositions(this.spaceCovered);
+		Assert.assertEquals(probabilityCount, this.probabilityCount);
+	}
+}
