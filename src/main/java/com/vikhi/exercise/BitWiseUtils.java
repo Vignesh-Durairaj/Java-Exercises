8360e7f3a08eb164286f616425945fad028e6c6e
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2018-12-02 19:39:35 +0530

    Renamed the param names as per standards

--- src/main/java/com/vikhi/exercise/BitWiseUtils.java
+++ src/main/java/com/vikhi/exercise/BitWiseUtils.java
@@ -2,15 +2,15 @@ package com.vikhi.exercise;
 
 public class BitWiseUtils {
 
-	public int getXorProduct (int a, int b) {
+	public int getXorProduct (int a, int N) {
 		
 		int[] xorTracker;
 	    if(a % 2 == 0) {
-	        xorTracker = new int[] {b, 1, b ^ 1, 0};
+	        xorTracker = new int[] {N, 1, N ^ 1, 0};
 	    } else {
-	        xorTracker = new int[] {a, a ^ b, a - 1, (a - 1) ^ b};
+	        xorTracker = new int[] {a, a ^ N, a - 1, (a - 1) ^ N};
 	    }
 	    		
-	    return xorTracker[(b - a) % 4];
+	    return xorTracker[(N - a) % 4];
 	}
 }
