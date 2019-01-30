c87317ffc38e6645f57e8c9e3888f7944e34031e
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-01-30 13:03:50 +0530

    Added another way to reverse a string using Lambdas

--- src/main/java/com/vikhi/exercise/Stringtest.java
+++ src/main/java/com/vikhi/exercise/Stringtest.java
@@ -1,5 +1,10 @@
 package com.vikhi.exercise;
 
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Stringtest {
 
     public String getReversedSentence (final String sentence) {
@@ -14,5 +19,25 @@ public class Stringtest {
 		}
 		
 		return builder.toString();
+    }
+    
+    public String getReversedSentenceWithStreams(final String sentence) {
+    	
+    	String[] strArr = sentence.split(" ");
+    	Supplier<String> strSupplier = new Supplier<String>() {
+
+    		int i = strArr.length - 1;
+    		
+			@Override
+			public String get() {
+				String myStr = strArr[i];
+				i --;
+				return myStr;
+			}
+    		
+		};
+    	
+		List<String> myList = Stream.generate(strSupplier).limit(strArr.length).collect(Collectors.toList());
+    	return String.join(" ", myList);
     }
 }
