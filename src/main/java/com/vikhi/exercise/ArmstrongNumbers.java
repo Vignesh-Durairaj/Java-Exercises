94f8d0be343c4bd06dde5149c73c02030f96c870
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-02-11 19:01:10 +0530

    Added a method for a bit faster evaluation Armstrong number generation

--- src/main/java/com/vikhi/exercise/ArmstrongNumbers.java
+++ src/main/java/com/vikhi/exercise/ArmstrongNumbers.java
@@ -4,24 +4,62 @@ import java.util.stream.IntStream;
 
 public class ArmstrongNumbers {
 
-	public boolean isArmstrongNumber(final int inNum) {
+	private int getLength(int inNum) {
+		int length = 0;
+		while (inNum > 0) {
+			length ++;
+			inNum /= 10;
+		}
+		return length;
+	}
+	
+	public int power(final int base, final int power) {
+		int product = 1;
+		int powerVal = power;
+		while (powerVal > 0){
+			product *= base;
+			powerVal --;
+		}
+		return product;
+	}
+	
+	public boolean isArmstrongNumberFaster(final int inNum) {
 		
 		int sumVal = 0;
 		int num = inNum;
-		int divisor = String.valueOf(inNum).length();
+		int divisor = getLength(inNum);
 		
 		while (num > 0) {
 			int rem = num % 10;
 			num /=  10;
-			sumVal += Math.pow(rem, divisor);
+			sumVal += power(rem, divisor);
 		}
 		return inNum == sumVal;
+	}
+	
+	public boolean isArmstrongNumber(final int inNum) {
+		char[] numCharArr = String.valueOf(inNum).toCharArray();
+		int divisor = numCharArr.length;
+		int sumVal = 0;
+		
+		for (char c: numCharArr) {
+			sumVal += Math.pow(Character.getNumericValue(c), divisor);
+		}
+		
+		return sumVal == inNum;
 	}
 	
 	public void getArmstrongNumbers() {
 		IntStream
 			.rangeClosed(1, Integer.MAX_VALUE)
 			.filter(this::isArmstrongNumber)
+			.forEach(System.out::println);
+	}
+
+	public void getArmstrongNumbersFaster() {
+		IntStream
+			.rangeClosed(1, Integer.MAX_VALUE)
+			.filter(this::isArmstrongNumberFaster)
 			.forEach(System.out::println);
 	}
 }
