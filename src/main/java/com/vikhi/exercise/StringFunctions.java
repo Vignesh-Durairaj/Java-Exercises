5f68926f2a2fc0ae0b831a9935bd6b726c315632
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-04-18 18:09:12 +0530

    Renamed the file as per standards

--- src/main/java/com/vikhi/exercise/StringFunctions.java
+++ src/main/java/com/vikhi/exercise/StringFunctions.java
@@ -6,7 +6,7 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class StringTest {
+public class StringFunctions {
 
     public String getReversedSentence (final String sentence) {
     	
