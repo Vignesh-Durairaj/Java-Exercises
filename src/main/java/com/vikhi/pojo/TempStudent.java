b5d3c13012bf846a6f588d32f23d86f297dd6e18
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-07-06 13:03:51 +0800

    Getter and setter testing added

--- src/main/java/com/vikhi/pojo/TempStudent.java
+++ src/main/java/com/vikhi/pojo/TempStudent.java
@@ -26,8 +26,44 @@ public class TempStudent {
 		this.mobileNumbers = student.getMobileNumber();
 	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public List<MobileNumber> getMobileNumbers() {
+		return mobileNumbers;
+	}
+
+	public void setMobileNumbers(List<MobileNumber> mobileNumbers) {
+		this.mobileNumbers = mobileNumbers;
 	}
 	
 }
