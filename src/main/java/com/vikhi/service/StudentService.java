f5c958d2b196fa8d02ac5356042d785c965f8f90
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-08-26 18:53:07 +0530

    Updated the service method

--- src/main/java/com/vikhi/service/StudentService.java
+++ src/main/java/com/vikhi/service/StudentService.java
@@ -57,6 +57,7 @@ public class StudentService {
 	public List<String> getStudentNameList(List<Student> students) {
 		return students
 				.stream()
+				.sorted()
 				.map(Student::getName)
 				.collect(Collectors.toList());
 	}
