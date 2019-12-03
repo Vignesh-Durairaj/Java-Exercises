cd9442c4e6bf7464405061b3fb4514eb51dea729
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-12-03 16:32:07 +0800

    Reducing cyclomatic complexity

--- src/main/java/com/vikhi/exercise/scenario/AlienLanguageTranslator.java
+++ src/main/java/com/vikhi/exercise/scenario/AlienLanguageTranslator.java
@@ -6,33 +6,19 @@ public class AlienLanguageTranslator implements LanguageTranslator {
 
 	@Override
 	public String fromDefaultLanguage(String languageTokens) {
-		char[] inArr = languageTokens.toCharArray();
-		StringBuilder builder = new StringBuilder();
-		
-		for (char c : inArr) {
-			int asciiVal = (int)c; 
-			int translateVal = 0;
-			if ((asciiVal >= 65 && asciiVal <= 90) || 
-					asciiVal >= 97 && asciiVal <= 122) {
-				if (asciiVal <= 90) {
-					translateVal = (asciiVal > 87) ? (asciiVal - 23) : asciiVal + 3;
-				} else {
-					translateVal = (asciiVal > 119) ? (asciiVal - 23) : asciiVal + 3;
-				}
-				builder.append((char)translateVal);
-			} else {
-				builder.append(c);
-			}
-		}
-		return builder.toString();
+		return getTranslation(languageTokens, true);
 	}
 
 	@Override
 	public String toDefaultLanguage(String languageTokens) {
+		return getTranslation(languageTokens, false);
+	}
+	
+	private String getTranslation(final String languageTokens, final boolean isFromDefaultLanguage) {
 		char[] inArr = languageTokens.toCharArray();
 		StringBuilder builder = new StringBuilder();
 		for (char c : inArr) {
-			builder.append(getTransformedCharacter(c, false));
+			builder.append(getTransformedCharacter(c, isFromDefaultLanguage));
 		}
 		return builder.toString();
 	}
@@ -48,27 +34,24 @@ public class AlienLanguageTranslator implements LanguageTranslator {
 	}
 	
 	private int getTranslatedValue(final int asciiVal, final boolean isFromDefaultLanguage) {
-		int translateVal = 0;
-		if (asciiVal <= 90) {
-			if (isFromDefaultLanguage) {
-				translateVal = (asciiVal > 119) ? (asciiVal - 23) : asciiVal + 3;
-			} else {
-				translateVal = (asciiVal < 68) ? (asciiVal + 23) : asciiVal - 3;
-			}
-			
-		} else {
-			if (isFromDefaultLanguage) {
-				translateVal = (asciiVal > 87) ? (asciiVal - 23) : asciiVal + 3;
-			} else {
-				translateVal = (asciiVal < 100) ? (asciiVal + 23) : asciiVal - 3;
-			}
-			
-		}
-		
-		return translateVal;
+		return asciiVal <= 90 ? 
+				getTranslatedValueForNearEnd(asciiVal, isFromDefaultLanguage) : 
+					getTranslatedValueForFarEnd(asciiVal, isFromDefaultLanguage);
 	}
 	
 	private int getTranslatedValueForNearEnd(final int asciiVal, final boolean isFromDefaultLanguage) {
-		return 1;
+		if (isFromDefaultLanguage) {
+			return (asciiVal > 87) ? (asciiVal - 23) : asciiVal + 3;
+		} else {
+			return (asciiVal < 68) ? (asciiVal + 23) : asciiVal - 3;
+		}
+	}
+	
+	private int getTranslatedValueForFarEnd(final int asciiVal, final boolean isFromDefaultLanguage) {
+		if (isFromDefaultLanguage) {
+			return (asciiVal > 119) ? (asciiVal - 23) : asciiVal + 3;
+		} else {
+			return (asciiVal < 100) ? (asciiVal + 23) : asciiVal - 3;
+		}
 	}
 }
