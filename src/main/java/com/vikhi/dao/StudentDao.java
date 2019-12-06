97b52d9cbbd4704716b05bddedbd019ff8272293
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-12-06 13:17:57 +0800

    Organized imports

--- src/main/java/com/vikhi/dao/StudentDao.java
+++ src/main/java/com/vikhi/dao/StudentDao.java
@@ -1,5 +1,7 @@
 package com.vikhi.dao;
 
+import static com.vikhi.util.ConstantsHelper.SYMBOL_BLANK;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,6 +21,6 @@ public class StudentDao {
 		return Arrays.asList(
 				new Student(1, "Kumar", 25, addressDao.getAddressByZipCode("641005"), mobileNumberDao.getMobileNumberByCountry("91")), 
 				new Student(2, "Patel", 50, addressDao.getAddressByZipCode("600100"), Arrays.asList(mobileNumberDao.getMobileNumber("91", "9443990380"))), 
-				new Student(3, "Singh", 34, addressDao.getAddressByZipCode(""), mobileNumberDao.getMobileNumberByCountry(""), false));
+				new Student(3, "Singh", 34, addressDao.getAddressByZipCode(SYMBOL_BLANK), mobileNumberDao.getMobileNumberByCountry(""), false));
 	}
 }
