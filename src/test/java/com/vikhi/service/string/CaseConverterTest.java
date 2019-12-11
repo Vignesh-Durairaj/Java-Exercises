d11dc68eaaa0b071e974b0cc3249cb49425facff
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-12-11 21:04:33 +0800

    Final Implementation

--- src/test/java/com/vikhi/service/string/CaseConverterTest.java
+++ src/test/java/com/vikhi/service/string/CaseConverterTest.java
@@ -1,7 +1,9 @@
 package com.vikhi.service.string;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,6 +11,8 @@ import com.vikhi.exercise.string.CaseConverter;
 
 public class CaseConverterTest {
 
+	private static final String MSG_EXCEPTION_EXPECTED = "An Exception is expected and should not execute this line of code";
+	private final Logger logger = Logger.getLogger(this.getClass());
 	private CaseConverter converter;
 	
 	@Before
@@ -39,12 +43,67 @@ public class CaseConverterTest {
 	public void testConvertToKebabString() {
 		assertEquals("the-stealth-warrior", converter.convertToKebabCase("TheStealthWarrior"));
 		assertEquals("the-stealth-warrior", converter.convertToKebabCase("theStealthWarrior"));
+		assertEquals("the-stealth-warrior", converter.convertToKebabCase("The-stealth-warrior"));
+		assertEquals("a", converter.convertToKebabCase("A"));
+		assertEquals("", converter.convertToKebabCase(""));
 	}
 	
 	@Test
 	public void testConvertToSnakeString() {
-		assertEquals("the_stealth_warrior", converter.convertToCamelCase("TheStealthWarrior"));
-		assertEquals("the_stealth_warrior", converter.convertToCamelCase("theStealthWarrior"));
+		assertEquals("the_stealth_warrior", converter.convertToSnakeCase("TheStealthWarrior"));
+		assertEquals("the_stealth_warrior", converter.convertToSnakeCase("theStealthWarrior"));
+		assertEquals("the_stealth_warrior", converter.convertToSnakeCase("The_stealth_warrior"));
+		assertEquals("a", converter.convertToSnakeCase("A"));
+		assertEquals("", converter.convertToSnakeCase(""));
+	}
+	
+	@Test
+	public void testConvertToUpperCaseKebabString() {
+		assertEquals("THE-STEALTH-WARRIOR", converter.convertToUpperCaseKebabCase("TheStealthWarrior"));
+		assertEquals("THE-STEALTH-WARRIOR", converter.convertToUpperCaseKebabCase("theStealthWarrior"));
+		assertEquals("THE-STEALTH-WARRIOR", converter.convertToUpperCaseKebabCase("The-stealth-warrior"));
+		assertEquals("A", converter.convertToUpperCaseKebabCase("a"));
+		assertEquals("", converter.convertToUpperCaseKebabCase(""));
+	}
+	
+	@Test
+	public void testConvertToUpperCaseSnakeString() {
+		assertEquals("THE_STEALTH_WARRIOR", converter.convertToUpperCaseSnakeCase("TheStealthWarrior"));
+		assertEquals("THE_STEALTH_WARRIOR", converter.convertToUpperCaseSnakeCase("theStealthWarrior"));
+		assertEquals("THE_STEALTH_WARRIOR", converter.convertToUpperCaseSnakeCase("The_stealth_warrior"));
+		assertEquals("A", converter.convertToUpperCaseSnakeCase("a"));
+		assertEquals("", converter.convertToUpperCaseSnakeCase(""));
+	}
+	
+	@Test
+	public void testConversionOfNullInputs() {
+		try {
+			assertEquals("the_stealth_warrior", converter.convertToCamelCase(null));
+			fail(MSG_EXCEPTION_EXPECTED);
+		} catch (IllegalArgumentException e) {
+			logger.error(e.getMessage(), e);
+		}
+		
+		try {
+			assertEquals("the_stealth_warrior", converter.convertToPascalCase(null));
+			fail(MSG_EXCEPTION_EXPECTED);
+		} catch (IllegalArgumentException e) {
+			logger.error(e.getMessage(), e);
+		}
+		
+		try {
+			assertEquals("the_stealth_warrior", converter.convertToKebabCase(null));
+			fail(MSG_EXCEPTION_EXPECTED);
+		} catch (IllegalArgumentException e) {
+			logger.error(e.getMessage(), e);
+		}
+		
+		try {
+			assertEquals("the_stealth_warrior", converter.convertToSnakeCase(null));
+			fail(MSG_EXCEPTION_EXPECTED);
+		} catch (IllegalArgumentException e) {
+			logger.error(e.getMessage(), e);
+		}
 	}
 	
 }
