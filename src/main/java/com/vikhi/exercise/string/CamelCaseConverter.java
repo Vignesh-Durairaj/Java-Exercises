3c20f03a26037457b6d96920d771fe64dd4948db
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-12-11 12:26:21 +0800

    Initial Setup

---
+++ src/main/java/com/vikhi/exercise/string/CamelCaseConverter.java
@@ -0,0 +1,12 @@
+package com.vikhi.exercise.string;
+
+public class CamelCaseConverter {
+
+	public String getCamelCaseString(final String inputString) {
+		return null;
+	}
+	
+	public String getSeparatedString(final String inputString, final char separator ) {
+		return null;
+	}
+}
