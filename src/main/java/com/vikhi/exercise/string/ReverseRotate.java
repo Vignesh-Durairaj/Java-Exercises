c803f2402b43d706389bf8c0f3ae3654ecbc7d73
Vignesh Durairaj <vignesh87.srkv@gmail.com>
2019-12-21 13:34:59 +0800

    Initial Setup

---
+++ src/main/java/com/vikhi/exercise/string/ReverseRotate.java
@@ -0,0 +1,20 @@
+package com.vikhi.exercise.string;
+
+/**
+ * The input is a string 'string' of digits. 
+ * Cut the string into chunks (a chunk here is a substring of the initial string) of size 'size' (ignore the last chunk if its size is less than 'size').
+
+ * If a chunk represents an integer such as the sum of the cubes of its digits is divisible by 2, reverse that chunk; 
+ * otherwise rotate it to the left by one position. Put together these modified chunks and return the result as a string.
+
+ * @author Vignesh_Durairaj
+ *
+ */
+public class ReverseRotate {
+
+	public String revRot(String string, int size) {
+		
+		
+		return null;
+	}
+}
